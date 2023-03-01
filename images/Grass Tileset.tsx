ffdecb8b7edb5dc2ce94683e7ac9d4d2cb97ad56
Romain-Ber <romain.bernier@live.fr>
2023-03-01 21:00:52 +0100

    restruct

---
+++ images/Grass Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Grass Tileset" tilewidth="64" tileheight="64" tilecount="256" columns="16">
+ <image source="tileset/Grass Tileset.png" trans="000000" width="1024" height="1024"/>
+</tileset>
